a216f38c9b649f9dcbbfd8d443d939770f951562
Bill Huang <a1245bill@yahoo.com.tw>
2019-08-04 16:01:33 +0800

    create new files

---
+++ content/images/test1.java
@@ -0,0 +1,16 @@
+public class test1{
+	public static void main(String[] args) {
+	int m = 1000;
+	int n = 495;
+	int y,z;
+
+		for(int a = 1;a <=495; a++){
+		y= m%a; && z= n%a ;
+			if(y==z){
+				System.out.println(a);
+				}
+		
+		}
+	}
+}	
+
