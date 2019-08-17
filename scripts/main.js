dd7196756a3c0d25333a203db932c07e78bcea85
Bill Huang <a1245bill@yahoo.com.tw>
2019-08-17 23:59:18 +0800

    add showBigImgInfo

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
-    $('.item').click(
-         $('#body').addClass('show')
-    );
+function ShowInfo(number) {
+     $('.showProduct').css('display','flex');
+     $('.showProduct').addClass('show');
+     
+};
+
+function close(){
+     $('.showProduct').removeClass('show');
+     console.log(123);
+};
+
